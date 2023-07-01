077910f2499f76ff55032ac59eeddd6ea0bb7c67
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-07-01 03:15:01 +0300

    fix date filter issue

--- app/Http/Livewire/ExpensesReport.php
+++ app/Http/Livewire/ExpensesReport.php
@@ -20,43 +20,44 @@ class ExpensesReport extends Component
     public function mount()
     {
         $this->months =
-        Transaction::selectRaw('DATE_FORMAT(date, "%m-%Y") as month')
-        ->groupBy('month')
-        ->get();
+            Transaction::selectRaw('DATE_FORMAT(date, "%Y-%m") as month')
+            ->where('target_type', 'App\Models\Category')
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
 
-        $this->selected_month = date('m');
-        $this->selected_year = date('Y');
-        $this->selected_row = $this->selected_month . '-' . $this->selected_year;
+        $this->selected_month = explode('-', $this->months->last()->month)[1];
+        $this->selected_year = explode('-', $this->months->last()->month)[0];
+        $this->selected_row =   $this->selected_year . '-' . $this->selected_month;
 
         $this->getData();
-
     }
 
-    public function getData() {
+    public function getData()
+    {
         $this->categories = Category::query()
-        ->where('country_id',session('current_country')->id)
-        ->where('type','expense')
-        ->where('category_id',null)
-        ->whereHas('incomingTransactions',function($q){
-            $q->whereMonth('date', $this->selected_month);
-            $q->whereYear('date', $this->selected_year);
-        })
-        ->orWhereHas('sub_categories.incomingTransactions',function($q){
-            $q->whereMonth('date', $this->selected_month);
-            $q->whereYear('date', $this->selected_year);
-        })
-        ->with('incomingTransactions')
-        ->with('outgoingTransactions')
-        ->with('sub_categories.incomingTransactions')
-        ->with('sub_categories.outgoingTransactions')
-        ->get()
-        ;
+            ->where('country_id', session('current_country')->id)
+            ->where('type', 'expense')
+            ->where('category_id', null)
+            ->whereHas('incomingTransactions', function ($q) {
+                $q->whereMonth('date', $this->selected_month);
+                $q->whereYear('date', $this->selected_year);
+            })
+            ->orWhereHas('sub_categories.incomingTransactions', function ($q) {
+                $q->whereMonth('date', $this->selected_month);
+                $q->whereYear('date', $this->selected_year);
+            })
+            ->with('incomingTransactions')
+            ->with('outgoingTransactions')
+            ->with('sub_categories.incomingTransactions')
+            ->with('sub_categories.outgoingTransactions')
+            ->get();
     }
 
     public function updatedSelectedRow()
     {
-        $this->selected_month = explode('-', $this->selected_row)[0];
-        $this->selected_year = explode('-', $this->selected_row)[1];
+        $this->selected_month = explode('-', $this->selected_row)[1];
+        $this->selected_year = explode('-', $this->selected_row)[0];
         $this->getData();
     }
 
