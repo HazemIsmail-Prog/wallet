7e39b048931c9ccb5a32a377cced7b427948a84d
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-07-01 03:26:25 +0300

    add all to date filter

--- app/Http/Livewire/ExpensesReport.php
+++ app/Http/Livewire/ExpensesReport.php
@@ -9,13 +9,10 @@ use Livewire\Component;
 class ExpensesReport extends Component
 {
 
-    public $categories;
     public $months;
     public $selected_row;
     public $selected_month;
     public $selected_year;
-
-
 
     public function mount()
     {
@@ -25,44 +22,40 @@ class ExpensesReport extends Component
             ->groupBy('month')
             ->orderBy('month')
             ->get();
-
-        $this->selected_month = explode('-', $this->months->last()->month)[1];
-        $this->selected_year = explode('-', $this->months->last()->month)[0];
-        $this->selected_row =   $this->selected_year . '-' . $this->selected_month;
-
-        $this->getData();
     }
 
-    public function getData()
+    public function updatedSelectedRow()
     {
-        $this->categories = Category::query()
+        if ($this->selected_row) {
+            $this->selected_month = explode('-', $this->selected_row)[1];
+            $this->selected_year = explode('-', $this->selected_row)[0];
+        }
+    }
+
+    public function render()
+    {
+        $categories = Category::query()
             ->where('country_id', session('current_country')->id)
             ->where('type', 'expense')
             ->where('category_id', null)
-            ->whereHas('incomingTransactions', function ($q) {
-                $q->whereMonth('date', $this->selected_month);
-                $q->whereYear('date', $this->selected_year);
-            })
-            ->orWhereHas('sub_categories.incomingTransactions', function ($q) {
-                $q->whereMonth('date', $this->selected_month);
-                $q->whereYear('date', $this->selected_year);
+            ->when($this->selected_row, function ($q) {
+                $q->whereHas('incomingTransactions', function ($q) {
+                    $q->whereMonth('date', $this->selected_month);
+                    $q->whereYear('date', $this->selected_year);
+                });
+                $q->orWhereHas('sub_categories.incomingTransactions', function ($q) {
+                    $q->whereMonth('date', $this->selected_month);
+                    $q->whereYear('date', $this->selected_year);
+                });
             })
             ->with('incomingTransactions')
             ->with('outgoingTransactions')
             ->with('sub_categories.incomingTransactions')
             ->with('sub_categories.outgoingTransactions')
             ->get();
-    }
 
-    public function updatedSelectedRow()
-    {
-        $this->selected_month = explode('-', $this->selected_row)[1];
-        $this->selected_year = explode('-', $this->selected_row)[0];
-        $this->getData();
-    }
-
-    public function render()
-    {
-        return view('livewire.expenses-report');
+        return view('livewire.expenses-report', [
+            'categories' => $categories
+        ]);
     }
 }
--- resources/views/livewire/expenses-report.blade.php
+++ resources/views/livewire/expenses-report.blade.php
@@ -3,7 +3,8 @@
     <div class="p-4 flex items-center justify-between dark:border-red-400 border-red-600 border-b">
         <div class=" text-red-600 dark:text-red-400 uppercase font-extrabold  ">expenses
         </div>
-        <select wire:ignore wire:model="selected_row" class="border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+        <select wire:ignore wire:model="selected_row" class=" text-center border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+            <option value="">All</option>
             @foreach ($months as $month)
                 <option value="{{ $month->month }}">{{ $month->month }}</option>
             @endforeach
