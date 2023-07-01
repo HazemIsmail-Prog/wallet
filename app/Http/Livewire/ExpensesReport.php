bbf5a5a4fb893b9c6b141ae61f89f5c31b7b0b92
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-07-01 22:51:21 +0300

    fix date filter issue in expenses report

--- app/Http/Livewire/ExpensesReport.php
+++ app/Http/Livewire/ExpensesReport.php
@@ -35,27 +35,43 @@ class ExpensesReport extends Component
     public function render()
     {
         $categories = Category::query()
+            ->select('id', 'name', 'category_id')
             ->where('country_id', session('current_country')->id)
             ->where('type', 'expense')
-            ->where('category_id', null)
-            ->when($this->selected_row, function ($q) {
-                $q->whereHas('incomingTransactions', function ($q) {
+            ->withSum(['incomingTransactions' => function ($q) {
+                $q->when($this->selected_row, function ($q) {
                     $q->whereMonth('date', $this->selected_month);
                     $q->whereYear('date', $this->selected_year);
                 });
-                $q->orWhereHas('sub_categories.incomingTransactions', function ($q) {
-                    $q->whereMonth('date', $this->selected_month);
-                    $q->whereYear('date', $this->selected_year);
-                });
-            })
-            ->with('incomingTransactions')
-            ->with('outgoingTransactions')
-            ->with('sub_categories.incomingTransactions')
-            ->with('sub_categories.outgoingTransactions')
+            }], 'amount')
             ->get();
 
+
+        $sub_categories = $categories->whereNotNull('category_id');
+        $parent_categories = $categories->whereNull('category_id');
+        
+        $final_list = [];
+        foreach($parent_categories as $index => $parent_category)
+        {
+            $final_list[$index]['id'] = $parent_category->id;
+            $final_list[$index]['name'] = $parent_category->name;
+            $final_list[$index]['amount'] = $parent_category->incoming_transactions_sum_amount;
+            foreach($sub_categories as $sub_index => $sub_category)
+            {
+                if($sub_category->category_id == $parent_category->id)
+                {
+                    $final_list[$index]['amount'] += $sub_category->incoming_transactions_sum_amount;
+                    $final_list[$index]['sub_categories'][$sub_index] = [
+                        'id' => $sub_category->id,
+                        'name' => $sub_category->name,
+                        'amount' => $sub_category->incoming_transactions_sum_amount,
+                    ];
+                }
+            }
+        }
+
         return view('livewire.expenses-report', [
-            'categories' => $categories
+            'categories' => collect($final_list)
         ]);
     }
 }
--- resources/views/livewire/expenses-report.blade.php
+++ resources/views/livewire/expenses-report.blade.php
@@ -3,34 +3,31 @@
     <div class="p-4 flex items-center justify-between dark:border-red-400 border-red-600 border-b">
         <div class=" text-red-600 dark:text-red-400 uppercase font-extrabold  ">expenses
         </div>
-        <select wire:ignore wire:model="selected_row" class=" text-center border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+        <select wire:ignore wire:model="selected_row"
+            class=" text-center border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
             <option value="">All</option>
             @foreach ($months as $month)
                 <option value="{{ $month->month }}">{{ $month->month }}</option>
             @endforeach
         </select>
     </div>
-    @foreach ($categories->sortByDesc('total') as $category)
+    @foreach ($categories->where('amount', '>', 0)->sortByDesc('amount') as $category)
         <div x-data="{ expanded: false }">
-            @if ($category->total + $category->sub_categories->sum('total') > 0)
-                <div @click="expanded=!expanded"
-                    class=" font-bold cursor-pointer flex items-center justify-between p-4 bg-white dark:bg-gray-800">
-                    <div>{{ $category->name }}</div>
+            <div @click="expanded=!expanded"
+                class=" font-bold cursor-pointer flex items-center justify-between p-4 bg-white dark:bg-gray-800">
+                <div>{{ $category['name'] }}</div>
+                <div>
+                    {{ number_format($category['amount'] / session('current_country')->factor, session('current_country')->decimal_points) }}
+                </div>
+            </div>
+            @foreach (collect(@$category['sub_categories'])->where('amount', '>', 0)->sortByDesc('amount') as $sub_category)
+                <div x-show="expanded" class=" flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-900">
+                    <div>{{ $sub_category['name'] }}</div>
                     <div>
-                        {{ number_format($category->total, session('current_country')->decimal_points) }}
+                        {{ number_format($sub_category['amount'] / session('current_country')->factor, session('current_country')->decimal_points) }}
                     </div>
                 </div>
-                @foreach ($category->sub_categories->sortByDesc('total') as $sub_category)
-                    @if ($sub_category->total > 0)
-                        <div x-show="expanded"
-                            class=" flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-900">
-                            <div>{{ $sub_category->name }}</div>
-                            <div>{{ number_format($sub_category->total, session('current_country')->decimal_points) }}
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            @endif
+            @endforeach
         </div>
     @endforeach
 </div>
