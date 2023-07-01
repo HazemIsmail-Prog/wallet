2488c846b22f7dc00b5f149c53c397d875598112
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-07-01 04:44:29 +0300

    add filter to stransactions index

--- app/Http/Livewire/TransactionIndex.php
+++ app/Http/Livewire/TransactionIndex.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
+use App\Models\Contact;
 use App\Models\Transaction;
+use App\Models\Wallet;
 use Livewire\Component;
 
 class TransactionIndex extends Component
@@ -13,15 +16,65 @@ class TransactionIndex extends Component
     ];
 
     public $days = [];
+    public $filter;
 
     public function mount()
     {
-        $this->days = Transaction::query()
-        ->where('country_id',session('current_country')->id)
-        ->with('target')
-        ->with('source')
-        ->get() ?? [];
+        $this->filter = [
+            'expense_list' => Category::where('country_id', session('current_country')->id)->where('type', 'expense')->orderBy('name')->get()->toArray(),
+            'selected_expense' => '',
+            'income_list' => Category::where('country_id', session('current_country')->id)->where('type', 'income')->orderBy('name')->get()->toArray(),
+            'selected_income' => '',
+            'wallet_list' => Wallet::where('country_id', session('current_country')->id)->orderBy('name')->get()->toArray(),
+            'selected_wallet' => '',
+            'contact_list' => Contact::where('country_id', session('current_country')->id)->orderBy('name')->get()->toArray(),
+            'selected_contact' => '',
+        ];
+
+        $this->getData();
     }
+
+    public function getData()
+    {
+        $this->days = Transaction::query()
+            ->where('country_id', session('current_country')->id)
+            ->when($this->filter['selected_expense'] , function($q){
+                $q->where('target_type','App\Models\Category');
+                $q->where('target_id',$this->filter['selected_expense']);
+            })
+            ->when($this->filter['selected_income'] , function($q){
+                $q->where('source_type','App\Models\Category');
+                $q->where('source_id',$this->filter['selected_income']);
+            })
+            ->when($this->filter['selected_wallet'] , function($q){
+                $q->where(function($q){
+
+                    $q->where('source_type','App\Models\Wallet');
+                    $q->where('source_id',$this->filter['selected_wallet']);
+                    $q->orWhere(function($q){
+                        $q->where('target_type','App\Models\Wallet');
+                        $q->where('target_id',$this->filter['selected_wallet']);
+                    });
+                });
+            })
+            ->when($this->filter['selected_contact'] , function($q){
+                $q->where('source_type','App\Models\Contact');
+                $q->where('source_id',$this->filter['selected_contact']);
+                $q->orWhere(function($q){
+                    $q->where('target_type','App\Models\Contact');
+                    $q->where('target_id',$this->filter['selected_contact']);
+                });
+            })
+            ->with('target')
+            ->with('source')
+            ->get() ?? [];
+    }
+
+    public function updatedFilter()
+    {
+        $this->getData();
+    }
+
 
     public function delete(Transaction $transaction)
     {
--- resources/views/livewire/transaction-index.blade.php
+++ resources/views/livewire/transaction-index.blade.php
@@ -4,7 +4,46 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-md mx-auto p-3 pb-16 flex flex-col gap-4">
+    <div class="max-w-md mx-auto p-3 pb-16 flex flex-col gap-3">
+        <div x-data="{ expanded: false }" class=" w-full text-end ">
+            <button @click="expanded=!expanded" class=" p-2 rounded-lg text-gray-500 dark:text-gray-400 bg-transparent dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                </svg>
+            </button>
+
+            <div x-show="expanded" class=" rounded-lg mt-2 border dark:border-none p-3">
+                <div class=" flex flex-col gap-2">
+                    <select wire:model="filter.selected_wallet" class=" text-center border  text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-transparent dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                        <option value="">Select Wallet</option>
+                        @foreach ($filter['wallet_list'] as $row)
+                            <option value="{{ $row['id'] }}">{{ $row['name'] }}</option>
+                        @endforeach
+                    </select>
+                    <select wire:model="filter.selected_expense" class=" text-center border  text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-transparent dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                        <option value="">Select Expenses</option>
+                        @foreach ($filter['expense_list'] as $row)
+                            <option value="{{ $row['id'] }}">{{ $row['name'] }}</option>
+                        @endforeach
+                    </select>
+                    <select wire:model="filter.selected_income" class=" text-center border  text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-transparent dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                        <option value="">Select Income</option>
+                        @foreach ($filter['income_list'] as $row)
+                            <option value="{{ $row['id'] }}">{{ $row['name'] }}</option>
+                        @endforeach
+                    </select>
+                    <select wire:model="filter.selected_contact" class=" text-center border  text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 bg-transparent dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                        <option value="">Select Contact</option>
+                        @foreach ($filter['contact_list'] as $row)
+                            <option value="{{ $row['id'] }}">{{ $row['name'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+        </div>
         @foreach ($days->sortByDesc('date')->groupBy('date') as $day => $transactions)
             <div
                 class=" border dark:border-none rounded-lg dark:bg-gray-800 text-gray-800 dark:text-gray-400 divide-y-2 dark:divide-gray-900 shadow-lg overflow-clip">
